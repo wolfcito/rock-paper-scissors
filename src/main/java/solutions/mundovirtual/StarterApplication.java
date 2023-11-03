7393b49c43b125dc2cab5cd1ca8b848e4a9cdacd
Luis Fernando <guffenix@gmail.com>
2023-11-02 22:17:02 -0500

    chore: legacy game refactor with testing strategy example

--- src/main/java/solutions/mundovirtual/StarterApplication.java
+++ src/main/java/solutions/mundovirtual/StarterApplication.java
@@ -1,15 +1,13 @@
 package solutions.mundovirtual;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"solutions.mundovirtual"})
-@SpringBootApplication
 public class StarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StarterApplication.class, args);
+        LegacyGame game = new LegacyGame();
+        game.play();
     }
 
 }
